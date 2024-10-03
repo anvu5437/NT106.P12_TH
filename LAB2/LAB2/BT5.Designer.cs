068e54d9de2e4ec061db3ba8b85351bdae30fc4d
Le Chanh An <161144795+LeChanhAn@users.noreply.github.com>
2024-10-03 10:44:36 +0700

    AAN-create BT5

--- LAB2/LAB2/BT5.Designer.cs
+++ LAB2/LAB2/BT5.Designer.cs
@@ -28,12 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(250, 426);
+            this.treeView1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(268, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(268, 318);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(400, 120);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "BT5";
+            this.ClientSize = new System.Drawing.Size(680, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "File Browser";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
--- LAB2/LAB2/BT5.cs
+++ LAB2/LAB2/BT5.cs
@@ -12,9 +12,94 @@ namespace LAB2
 {
     public partial class BT5 : Form
     {
+
         public BT5()
         {
             InitializeComponent();
+            LoadDrives();
+            treeView1.NodeMouseClick += TreeView1_NodeMouseClick;
+        }
+
+        private void LoadDrives()
+        {
+            foreach (var drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady)
+                {
+                    TreeNode node = new TreeNode(drive.Name);
+                    node.Tag = drive.RootDirectory;
+                    treeView1.Nodes.Add(node);
+                    LoadDirectories(node);
+                }
+            }
+        }
+
+        private void LoadDirectories(TreeNode node)
+        {
+            DirectoryInfo directory = (DirectoryInfo)node.Tag;
+
+            try
+            {
+                foreach (var dir in directory.GetDirectories())
+                {
+                    TreeNode childNode = new TreeNode(dir.Name);
+                    childNode.Tag = dir;
+                    node.Nodes.Add(childNode);
+                    LoadDirectories(childNode); // Đệ quy để tải các thư mục con
+                }
+
+                foreach (var file in directory.GetFiles())
+                {
+                    TreeNode fileNode = new TreeNode(file.Name);
+                    fileNode.Tag = file;
+                    node.Nodes.Add(fileNode);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // Không có quyền truy cập vào thư mục
+            }
+        }
+
+        private void TreeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Node.Tag is FileInfo fileInfo)
+            {
+                DisplayFileContent(fileInfo);
+            }
+            else if (e.Node.Tag is DirectoryInfo)
+            {
+                // Chọn thư mục
+                richTextBox1.Clear();
+                pictureBox1.Image = null; // Xóa hình ảnh trước
+            }
+        }
+
+        private void DisplayFileContent(FileInfo fileInfo)
+        {
+            string extension = fileInfo.Extension.ToLower();
+
+            if (extension == ".txt")
+            {
+                // Đọc nội dung file văn bản
+                using (StreamReader sr = new StreamReader(fileInfo.FullName))
+                {
+                    richTextBox1.Text = sr.ReadToEnd();
+                }
+                pictureBox1.Image = null; // Xóa hình ảnh
+            }
+            else if (extension == ".jpg" || extension == ".jpeg" || extension == ".png")
+            {
+                // Hiển thị hình ảnh
+                pictureBox1.Image = Image.FromFile(fileInfo.FullName);
+                richTextBox1.Clear(); // Xóa nội dung văn bản
+            }
+            else
+            {
+                richTextBox1.Clear();
+                pictureBox1.Image = null; // Xóa hình ảnh
+                MessageBox.Show("Chỉ hỗ trợ file văn bản và hình ảnh!");
+            }
         }
     }
 }
