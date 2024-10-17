894a35dc7171e81b5e694d6756b4c223e4fe2381
An Vũ <erogami5437@gmail.com>
2024-10-17 10:17:04 +0700

    anvu_Delete BT3 form and upload mainMenu

--- LAB3/LAB3/mainMenu.Designer.cs
+++ LAB3/LAB3/mainMenu.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "mainMenu";
+            button1 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(158, 58);
+            button1.Name = "button1";
+            button1.Size = new Size(153, 59);
+            button1.TabIndex = 0;
+            button1.Text = "BT1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(47, 172);
+            button3.Name = "button3";
+            button3.Size = new Size(153, 59);
+            button3.TabIndex = 2;
+            button3.Text = "BT4";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(259, 172);
+            button4.Name = "button4";
+            button4.Size = new Size(153, 59);
+            button4.TabIndex = 3;
+            button4.Text = "BT5";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // mainMenu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(466, 322);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button1);
+            Name = "mainMenu";
+            Text = "mainMenu";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
+        private Button button3;
+        private Button button4;
     }
 }
--- LAB3/LAB3/mainMenu.cs
+++ LAB3/LAB3/mainMenu.cs
@@ -16,5 +16,26 @@ namespace LAB3
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BT1_Dashboard bt1 = new BT1_Dashboard();
+            bt1.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            BT4_Dashboard bt1 = new BT4_Dashboard();
+            bt1.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            BT5_Dashboard bt1 = new BT5_Dashboard();
+            bt1.Show();
+            this.Hide();
+        }
     }
 }
