fb1b4c8c231f4ef280f9d66b1bece2be8c01f01b
An Vũ <erogami5437@gmail.com>
2024-10-17 08:36:51 +0700

    anvu_add forms component

---
+++ LAB3/LAB3/BT5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB3
+{
+    public partial class BT5 : Form
+    {
+        public BT5()
+        {
+            InitializeComponent();
+        }
+    }
+}
