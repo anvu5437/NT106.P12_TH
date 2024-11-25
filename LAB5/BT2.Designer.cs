f077b1d083e539fb2c0f0426bcb40fd03ddda1dc
khaipd18 <khaipd180405@gmail.com>
2024-11-25 15:59:31 +0700

    khaipd18 - BT2

--- LAB5/BT2.Designer.cs
+++ LAB5/BT2.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "BT2";
+            listView1 = new ListView();
+            EmailColl = new ColumnHeader();
+            FromColl = new ColumnHeader();
+            TimeColl = new ColumnHeader();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            loginBt = new Button();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { EmailColl, FromColl, TimeColl });
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.Location = new Point(12, 131);
+            listView1.MultiSelect = false;
+            listView1.Name = "listView1";
+            listView1.Size = new Size(793, 247);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // EmailColl
+            // 
+            EmailColl.Text = "Email";
+            EmailColl.Width = 375;
+            // 
+            // FromColl
+            // 
+            FromColl.Text = "Từ";
+            FromColl.Width = 250;
+            // 
+            // TimeColl
+            // 
+            TimeColl.Text = "Thời gian";
+            TimeColl.Width = 160;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Email:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(91, 44);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(267, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(91, 6);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(267, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // loginBt
+            // 
+            loginBt.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            loginBt.Location = new Point(174, 77);
+            loginBt.Name = "loginBt";
+            loginBt.Size = new Size(94, 29);
+            loginBt.TabIndex = 5;
+            loginBt.Text = "Đăng nhập";
+            loginBt.UseVisualStyleBackColor = true;
+            loginBt.Click += loginBt_Click;
+            // 
+            // BT2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(817, 390);
+            Controls.Add(loginBt);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(listView1);
+            Name = "BT2";
+            Text = "BT2";
+            FormClosing += BT2_FormClosing;
+            Load += BT2_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListView listView1;
+        private ColumnHeader EmailColl;
+        private ColumnHeader FromColl;
+        private ColumnHeader TimeColl;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button loginBt;
     }
 }
